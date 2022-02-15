4bfb7f633eff51a7f00affcdd9cb34fad8891b9c
mboyas <michael.boyas@kellogg.northwestern.edu>
2022-02-15 17:35:18 +0000

    work from class

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,4 +4,13 @@ class ContactsController < ApplicationController
     @contact = Contact.find(params["id"])
   end
 
+  def new
+    @contact = Contact.new
+    @contact.company_id = pa
+  end
+
+  def create
+    @contact = Contact.new(params["contact"])
+  end
+
 end
